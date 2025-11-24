eebb7b1852920d24fefa9fa3bfe1e2e5967c5cad
inaemin <inaemin@gmail.com>
2025-11-24 23:35:41 +0900

    feat: 앱 진입점 및 기본 구조
    - main.tsx, App.tsx 설정
    - 전역 CSS 스타일 설정

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import BumpContainer from "./components/bump-container";
+import Container from "./components/container";
+import TouchContainer from "./components/touch-container";
+import TransformContainer from "./components/transform-container";
+import { Grain, Warmth, WhiteOverlay, YummyColorGrading } from "./effects";
+import {
+  ArtifactTop,
+  CircularLight,
+  CircularLightArtifact,
+  Light,
+  TinyCircularLight,
+} from "./lights";
+
+function App() {
+  return (
+    <>
+      <Container>
+        <TransformContainer>
+          <BumpContainer>
+            <TouchContainer />
+          </BumpContainer>
+        </TransformContainer>
+      </Container>
+      <Grain />
+      <Light />
+      <CircularLight />
+      <ArtifactTop />
+      <CircularLightArtifact />
+      <TinyCircularLight />
+      <YummyColorGrading />
+      <Warmth />
+      {/* <WhiteOverlay /> */}
+    </>
+  );
+}
+
+export default App;
