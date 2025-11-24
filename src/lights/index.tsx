e9344ab7a2bb43e9b73b06c03990815ccd55ca23
inaemin <inaemin@gmail.com>
2025-11-24 23:37:19 +0900

    feat: Lights 컴포넌트
    - conic-gradient로 빛 효과 구현
    - Light, CircularLight, TinyCircularLight
    - 렌즈 플레어 아티팩트

---
+++ src/lights/index.tsx
@@ -0,0 +1,21 @@
+import styles from "./style.module.css";
+
+export function Light() {
+  return <div className={styles.light} />;
+}
+
+export function CircularLight() {
+  return <div className={styles.circularLight} />;
+}
+
+export function TinyCircularLight() {
+  return <div className={styles.tinyCircularLight} />;
+}
+
+export function ArtifactTop() {
+  return <div className={styles.artifactTop} />;
+}
+
+export function CircularLightArtifact() {
+  return <div className={styles.circularLightArtifact} />;
+}
